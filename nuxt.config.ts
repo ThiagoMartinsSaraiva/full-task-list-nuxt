024731cb4f0620df66e784edfbd75aaa5f800358
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2025-02-26 02:02:16 -0300

    feat: configuring supabase and font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/supabase', '@nuxt/fonts'],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  },
 })
